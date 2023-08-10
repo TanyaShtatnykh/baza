c3e7a82629b58960d7dd27984f179a56379b86c8
Tanya Shtatnykh <tanya.shtatnykh@gmail.com>
2023-08-11 01:09:46 +0700

    Edits of two blocks

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -2,7 +2,6 @@
 
 
 
-
 $(function() {
   $('.header__menu-link').on('click', function() {
     $('main').addClass('open__page');
@@ -10,28 +9,17 @@ $(function() {
 });
 
 
-$('.slider__main-slide').slick({
+
+$('.slider__slide').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     fade: true,
-    asNavFor: '.slider__carousel',
     swipe: true,
-    adaptiveHeight: true
-});
-$('.slider__carousel').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider__main-slide',
+    adaptiveHeight: true,
     dots: true,
-    centerMode: true,
-    focusOnSelect: true,
-    dots: false,
-    swipe: true,
-    arrows: true,
-    variableWidth: true,
-    prevArrow: '<img src="img/prevArrow.svg" class="prev" alt="Назад">',
-    nextArrow: '<img src="img/prevArrow.svg" class="next" alt="Вперед">'
+    autoplay: true,
+    autoplaySpeed: 2000
 });
 $(function() {
   $('.welcome__link-scroll').on('click', function() {
--- src/blocks/slider/slider.js
+++ src/blocks/slider/slider.js
@@ -1,23 +1,11 @@
-$('.slider__main-slide').slick({
+$('.slider__slide').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     fade: true,
-    asNavFor: '.slider__carousel',
     swipe: true,
-    adaptiveHeight: true
-});
-$('.slider__carousel').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider__main-slide',
+    adaptiveHeight: true,
     dots: true,
-    centerMode: true,
-    focusOnSelect: true,
-    dots: false,
-    swipe: true,
-    arrows: true,
-    variableWidth: true,
-    prevArrow: '<img src="img/prevArrow.svg" class="prev" alt="Назад">',
-    nextArrow: '<img src="img/prevArrow.svg" class="next" alt="Вперед">'
+    autoplay: true,
+    autoplaySpeed: 2000
 });
